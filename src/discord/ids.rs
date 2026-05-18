430e168b3e56c72b80c9016f5f29b5c1dc991f02
Neo <chojs990222@gmail.com>
2026-05-18 23:19:01 +0900

    chore: add Twilight attribution for Discord IDs (#95)

--- src/discord/ids.rs
+++ src/discord/ids.rs
@@ -2,6 +2,13 @@ use std::{fmt, marker::PhantomData, num::NonZeroU64};
 
 use serde::{Deserialize, Deserializer, Serialize, Serializer, de};
 
+// Type adapted from Twilight <https://github.com/twilight-rs/twilight>
+// ISC License (ISC)
+// Copyright (c) 2025 Twilight Contributors
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
 #[repr(transparent)]
 pub struct Id<T> {
